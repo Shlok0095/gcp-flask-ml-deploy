44a247cf88f1653593a928572901d5ebbd445057
Shlok Kumar <153258841+Shlok0095@users.noreply.github.com>
2025-01-17 00:31:35 +0530

    Testing continous delivery
    Testing continous delivery

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
   """Return a friendly Http greeting"""
   print("I am inside hello world")
-  return "Hello World!"
+  return "Hello World! Continous Delivery"
 
 @app.route('/echo/<name>')
 def echo(name):
